feb31649f1b8132919b094533c3ca82e6bc3ce6d
Gonzalo Benítez <gonzalo.benitez@ingenieria.uner.edu.ar>
2025-12-09 16:25:56 -0300

    Se actualiza el .gitignore y se crea el proyecto "LedCodeNervio".

---
+++ firmware/projects/LedCodeNervio/README.md
@@ -0,0 +1,5 @@
+# Template
+
+Este proyecto contiene el código perteneciente a la adscripción de la cátedra de Histología y Anatomía del año 2025.
+
+Su estructura es la siguiente: (A completar)
---
+++ firmware/projects/LedCodeNervio/main/LedCodeNervio.c
@@ -0,0 +1,43 @@
+/*! @mainpage Template
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 12/09/2023 | Document creation		                         |
+ *
+ * @author Albano Peñalva (albano.penalva@uner.edu.ar)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/rmt_tx.h"
+#include "led_strip.h"
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	printf("Hello world!\n");
+}
+/*==================[end of file]============================================*/
